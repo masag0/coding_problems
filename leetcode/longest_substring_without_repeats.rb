ba66873e6858e7816bfd1b5259f1980edb2119e1
Edwin Shin <edwinshin@gmail.com>
2018-03-01 02:23:29 -0800

    Add O(n) solution to longest_substring_without_repeats

--- leetcode/longest_substring_without_repeats.rb
+++ leetcode/longest_substring_without_repeats.rb
@@ -1,23 +1,25 @@
 # @param {String} s
 # @return {Integer}
 def length_of_longest_substring(s)
-  substrings = []
-  for i in 0...s.length
-    for j in i...s.length
-      temp = {}
-      bool = true
-      s[i..j].each_char do |ch|
-        bool = false if temp[ch]
-          temp[ch] = true
-      end
-      substrings << s[i..j] if bool
+  hsh = {}
+  max = 0
+  i, j = 0, 0
+
+  while j < s.length && i < s.length
+    if hsh[s[j]]
+      i = hsh[s[j]] + 1 if hsh[s[j]] + 1 > i
     end
-  end
-  result = ""
-
-  substrings.each do |str|
-    result = str if str.length > result.length
+    hsh[s[j]] = j
+    max = s[i..j].length if s[i..j].length > max
+    j += 1
   end
 
-  result.length
+  max
 end
+
+p length_of_longest_substring('pwwkew')
+p length_of_longest_substring('dvdf')
+p length_of_longest_substring('abcabcbb')
+p length_of_longest_substring('bbbbbb')
+p length_of_longest_substring('abba')
+p length_of_longest_substring("tmmzuxt")
