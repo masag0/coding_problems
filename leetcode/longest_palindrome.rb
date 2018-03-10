5e0dea9f4804001edb82e101e4c36eadeaf431db
Edwin Shin <edwinshin@gmail.com>
2018-03-09 16:38:15 -0800

    Add longest palindrome

---
+++ leetcode/longest_palindrome.rb
@@ -0,0 +1,20 @@
+# @param {String} s
+# @return {String}
+# def longest_palindrome(s) #naive solution
+#     subs = []
+#     for i in 0..s.length
+#         for j in i..s.length
+#             subs << s[i..j] if s[i..j] == s[i..j].reverse
+#         end
+#     end
+#     p subs
+#     subs.max_by { |s| s.length }
+# end
+
+def longest_palindrome(s)
+
+end
+
+
+
+p longest_palindrome('babad')
