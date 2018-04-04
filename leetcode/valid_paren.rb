f733b568a97797d38b62c97befa9942d6002d971
Edwin Shin <edwinshin@gmail.com>
2018-04-03 21:04:10 -0700

    Complete valid paren

--- leetcode/valid_paren.rb
+++ leetcode/valid_paren.rb
@@ -1,5 +1,16 @@
 # @param {String} s
 # @return {Boolean}
 def is_valid(s)
+  hash = { '}' => '{', ']' => '[', ')' => '('}
+  stack = []
 
+  s.each_char do |ch|
+    if hash.has_value?(ch)
+      stack << ch
+    elsif hash.has_key?(ch)
+      return false if stack.pop != hash[ch]
+    end
+  end
+  
+  stack.empty? ? true : false
 end
