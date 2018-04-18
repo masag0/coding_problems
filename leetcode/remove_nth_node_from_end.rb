dc74f375bedff2b06c8d6ea8f28820cf53dadba6
Edwin Shin <edwinshin@gmail.com>
2018-04-17 21:09:50 -0700

    Add remove nth node from end

---
+++ leetcode/remove_nth_node_from_end.rb
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @param {Integer} n
+# @return {ListNode}
+def remove_nth_from_end(head, n)
+    hash = {}
+    curr = head
+    count = 0
+    while curr
+        hash[count] = curr
+        count += 1
+        curr = curr.next
+    end
+
+    if count <= 1
+        head.val = nil
+        return head
+    end
+    remove_node = hash[count-n]
+    prev = hash[count-n-1]
+    after = hash[count-n+1]
+    if remove_node == head
+        head = after
+    else
+        prev.next = after if prev
+        remove_node.next = nil
+    end
+
+    head
+end
